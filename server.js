8477dbb08246e18b9fd7282b56fbb5089a4abfd9
Mario Gastón Ferrufino <mferrufino@ibm.com>
2020-04-29 21:31:54 -0300

    First Commit2

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = require('./src/router');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use(router);
 
 app.listen(port, err => {
